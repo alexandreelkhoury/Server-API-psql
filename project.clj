0e9d7f27a401c0fe77574462f229213d03e39003
Alexandre El-Khoury <alexkhoury@hotmail.com>
2023-02-06 20:18:28 +0100

    upgrades

--- project.clj
+++ project.clj
@@ -11,7 +11,10 @@
                  [com.github.seancorfield/next.jdbc "1.3.847"]
                  [metosin/reitit "0.5.18"]
                  [cheshire "5.11.0"]
-                 [org.clojure/data.json "2.4.0"]]
+                 [org.clojure/data.json "2.4.0"]
+                 [com.github.seancorfield/next.jdbc "1.3.847"]
+                 [com.github.seancorfield/honeysql "2.4.972"]
+                 [integrant "0.8.0"]]
   :main ^:skip-aot http-kit-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
